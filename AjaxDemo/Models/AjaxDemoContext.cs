69ba24790ed20805f1a91683d32deb0cf9d07d10
Guest User <Guest@epicodus-50.local>
2017-10-25 09:45:19 -0700

    ajax demo project completed

---
+++ AjaxDemo/Models/AjaxDemoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxDemo.Models
+{
+    public class AjaxDemoContext : DbContext
+    {
+        public AjaxDemoContext() 
+        {
+          
+        }
+        public DbSet<Destination> Destinations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=Ajax_Demo;uid=root;pwd=root;");
+        }
+
+        public AjaxDemoContext(DbContextOptions<AjaxDemoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
